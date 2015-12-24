c1ca47300280e1ee8ea13b396c4c4869e8122fce
Nil Thacker <nil@nil.gs>
2015-12-23 23:36:55 -0800

    URL parameters

--- sinatra.rb
+++ sinatra.rb
@@ -7,3 +7,12 @@ end
 get '/about' do
   "nil is everywhere"
 end
+
+get '/greetings/:name' do
+  "Howdy, #{params[:name]}!"
+end
+
+get '/cities/:city/:greetings/:name' do
+  "Hey #{params[:name]}! Welcome to the #{params[:city]} greeting page!"
+  "<h1>We're really excited you're here!</h1>"
+end
